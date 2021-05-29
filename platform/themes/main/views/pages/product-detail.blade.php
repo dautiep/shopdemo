0a1bd8efbb7338c259a441b7f4ca777018f568df
dautiep <dautiep123@gmail.com>
2021-05-29 21:11:03 +0700

    update sources

--- platform/themes/main/views/pages/product-detail.blade.php
+++ platform/themes/main/views/pages/product-detail.blade.php
@@ -1,11 +1,38 @@
+
 <!-- PRODUCT DETAIL START -->
 <div class="section">
     <div class="line">
         <div class="margin">
             <!-- product image -->
             <div class="s-12 m-4 l-5 margin-bottom">
-                <img src="{{ RvMedia::getImageUrl($product->image)  }}" alt="{{ $product->name }}">
+
+                    <img id="expandedImg" style="width:100%" src="{{ Theme::asset()->url('img/products/son-kem-li.jpg') }}" />
+                    <!-- Close the image -->
+                    <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+
+                    <!-- Expanded image -->
+
+
+                    <!-- Image text -->
+                    <div id="imgtext"></div>
+
+                <div class="row">
+                    <div class="column">
+                        <img src="{{ RvMedia::getImageUrl($product->image)  }}" alt="Nature" onclick="myFunction(this);">
+                    </div>
+                    <div class="column">
+                        <img src="{{ RvMedia::getImageUrl($product->image)  }}" alt="Snow" onclick="myFunction(this);">
+                    </div>
+                    <div class="column">
+                        <img src="{{ Theme::asset()->url('img/products/son-kem-li.jpg') }}" alt="Mountains" onclick="myFunction(this);">
+                    </div>
+                    <div class="column">
+                        <img src="{{ Theme::asset()->url('img/products/son-kem-li.jpg') }}" onclick="myFunction(this);">
+                    </div>
+                </div>
+                <!-- <img src="{{ RvMedia::getImageUrl($product->image)  }}" alt="{{ $product->name }}"> -->
             </div>
+
             <!-- product detail -->
             <div class="s-12 m-8 l-7 padding-l-left">
                 <h2 class="text-primary product-name">{{ $product->name }}</h2>
@@ -18,6 +45,7 @@
                         <label>Số lượng:</label>
                         <input type="number" name="quanity" class="quantity" id="french-hens" value="1">
                     </div>
+
                 </div>
                 <div class="s-12 m-6 l-4">
                     <a href="" class="button">Thêm vào giỏ hàng</a>
@@ -26,4 +54,13 @@
         </div>
     </div>
 </div>
-<!-- PRODUCT DETAIL END -->
+<!-- PRODUCT DETAIL END  -->
+<script>
+    function myFunction(imgs) {
+        var expandImg = document.getElementById("expandedImg");
+        var imgText = document.getElementById("imgtext");
+        expandImg.src = imgs.src;
+        imgText.innerHTML = imgs.alt;
+        expandImg.parentElement.style.display = "block";
+    }
+</script>
